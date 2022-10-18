fdd90c7372bc92df279a0506423046d3b48ec515
abhishah <abhishah0961@gmail.com>
2022-10-18 15:47:17 +0530

    Setup: Typeorm & database

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,27 @@ import { AppService } from './app.service';
 import { PetsModule } from './pets/pets.module';
 import { ApolloDriver } from '@nestjs/apollo';
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core'
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Pet } from './pets/pets.entity';
 
 @Module({
-  imports: [GraphQLModule.forRoot({
-    playground: false,
-    plugins: [ApolloServerPluginLandingPageLocalDefault],
-    autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-    driver: ApolloDriver,
-  }),
+  imports: [
+    GraphQLModule.forRoot({
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault],
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      driver: ApolloDriver,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'nest_typeorm',
+      synchronize: true,
+      entities: [Pet]
+    }),
     PetsModule],
   controllers: [AppController],
   providers: [AppService],
